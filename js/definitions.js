528773eb310ed15720131eac8e2414787dad6296
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-02-26 16:34:14 -0800

    Aquadoc initial commit

---
+++ js/definitions.js
@@ -0,0 +1,89 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "Sample Models for Aquarium Protocols",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/dvnstrcklnd/aq-sample-models/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+      {
+
+        category: "Libraries",
+
+        contents: [
+
+          
+            {
+              name: 'AbstractSample',
+              path: 'libraries/AbstractSample' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'DNALibrary',
+              path: 'libraries/DNALibrary' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'OligoPool',
+              path: 'libraries/OligoPool' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'Primer',
+              path: 'libraries/Primer' + '.html',
+              type: "local-webpage"
+            },
+          
+
+        ]
+
+    },
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+        ]
+      }
+    
+
+  ]
+
+};
